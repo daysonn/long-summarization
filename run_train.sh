3aa360d2f8965d58fb86fe0b3caacb3221f60628
Dayson Nascimento <dayson.ncrn@gmail.com>
2022-11-17 16:25:46 -0300

    Subindo arquivos para treino reduzido em teste na cloud

---
+++ run_train.sh
@@ -0,0 +1,22 @@
+python run_summarization.py \
+--mode=train \
+--data_path=./data/val_data_pt.bin \
+--vocab_path=./data/vocab.vocab \
+--log_root=logroot \
+--exp_name=test-short \
+--hidden_dim=24 \
+--emb_dim=10 \
+--max_dec_steps=210 \
+--max_enc_steps=250 \
+--num_sections=3 \
+--max_section_len=1000 \
+--batch_size=4 \
+--vocab_size=5000 \
+--use_do=True \
+--optimizer=adagrad \
+--do_prob=0.25 \
+--hier=True \
+--split_intro=True \
+--fixed_attn=True \
+--legacy_encoder=False \
+--coverage=False
